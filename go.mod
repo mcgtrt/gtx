dedf4e812fc65592c15c1c8c5c689df0547df058
Maciej Marut <mcgtrt@gmail.com>
2024-05-10 14:42:12 +0100

    base layout with tailwind config

--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/phuslu/log v1.0.97
 )
+
+require github.com/a-h/templ v0.2.680 // indirect
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./**/*.html", 
+    "./**/*.templ", 
+    "./**/*.go",
+  ],
   theme: {
     extend: {},
     colors: {
